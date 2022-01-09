7435e8a4d661e9b1b6d6efa85a16b8879c64ed8a
Ilmar Uduste <ilmaruduste@gmail.com>
2022-01-09 21:47:25 +0200

    Add skybox and ref material

--- src/main.js
+++ src/main.js
@@ -15,13 +15,52 @@ scene.add(camera);
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.update();
 
-// var boxGeometry = new THREE.BoxGeometry(10, 10, 10);
-// var basicMaterial = new THREE.MeshBasicMaterial({color: 0x0095DD});
-// var cube = new THREE.Mesh(boxGeometry, basicMaterial);
-// // cube.position.x = -25;
-// cube.rotation.set(0.4, 0.2, 0);
-// scene.add(cube);
+// Skybox
+// https://codinhood.com/post/create-skybox-with-threejs
+// Cloudy sky from here: https://opengameart.org/content/sky-box-sunny-day
 
+function createPathStrings(filename) {
+  const basePath = "../ref/skybox/";
+  const baseFilename = basePath + filename;
+  const fileType = ".png";
+  const sides = ["front", "back", "top", "bottom", "right", "left"];
+  const pathStings = sides.map(side => {
+    return baseFilename + "_" + side + fileType;
+  });
+  return pathStings;
+}
+
+function createMaterialArray(filename) {
+  const skyboxImagepaths = createPathStrings(filename);
+  const materialArray = skyboxImagepaths.map(image => {
+    let texture = new THREE.TextureLoader().load(image);
+    return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+  });
+  return materialArray;
+}
+
+// skyboxImagePaths = [
+//   '../ref/skybox/skybox_front.png',
+//   '../ref/skybox/skybox_back.png',
+//   '../ref/skybox/skybox_top.png',
+//   '../ref/skybox/skybox_bottom.png',
+//   '../ref/skybox/skybox_right.png',
+//   '../ref/skybox/skybox_left.png'
+// ];
+// const materialArray = skyboxImagepaths.map(image => {
+//   let texture = new THREE.TextureLoader().load(image);
+//   return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+// });
+
+const skyboxImage = "skybox";
+const materialArray = createMaterialArray(skyboxImage);
+
+skyboxGeo = new THREE.BoxGeometry(100, 100, 100);
+skybox = new THREE.Mesh(skyboxGeo, materialArray);
+scene.add(skybox);
+
+
+// Geometry
 var guiObj = createGuiObject();
 var strangeGeometry = new THREE.DodecahedronGeometry(guiObj.geometrySize);
 var lambertMaterial = new THREE.MeshLambertMaterial({color: 0x0095DD});
@@ -39,25 +78,42 @@ var t = 0;
 function createPanel() {
   const panel = new dat.GUI( {width: 310});
   
-  const folder1 = panel.addFolder('Geometry');
-
+  const geometrySettings = panel.addFolder('Geometry');
+  const cameraSettings = panel.addFolder('Camera');
+  
   
   // folder1.add(dodecahedron.rotation, 'x', 0, 3.14 * 2);
   // folder1.add(dodecahedron.rotation, 'y', 0, 3.14 * 2);
   // folder1.add(dodecahedron.rotation, 'z', 0, 3.14 * 2);
-  folder1.add(dodecahedron.scale, 'x', 0, 10, 0.01).name('Width').listen();
-  folder1.add(dodecahedron.scale, 'y', 0, 10, 0.01).name('Height').listen();
-  folder1.add(dodecahedron.scale, 'z', 0, 10, 0.01).name('Length').listen();
-  folder1.add(guiObj, 'rotationSpeedX', 0, 0.2, 0.005).listen();
-  folder1.add(guiObj, 'rotationSpeedY', 0, 0.2, 0.005).listen();
-  folder1.add(guiObj, 'rotationSpeedZ', 0, 0.2, 0.005).listen();
-  folder1.add(guiObj, 'translationSpeedX', 0, 10, 0.01).listen();
-  folder1.add(guiObj, 'translationSpeedY', 0, 10, 0.01).listen();
-  folder1.add(guiObj, 'translationSpeedZ', 0, 10, 0.01).listen();
-  folder1.add(dodecahedron.material, 'wireframe').listen();
-  folder1.add(guiObj, 'resetCamera').name('Reset Camera');
-  folder1.add(guiObj, 'resetGeometry').name('Reset Geometry');
-  folder1.open();
+  
+  // Scale
+  geometrySettings.add(dodecahedron.scale, 'x', 0, 10, 0.01).name('Width').listen();
+  geometrySettings.add(dodecahedron.scale, 'y', 0, 10, 0.01).name('Height').listen();
+  geometrySettings.add(dodecahedron.scale, 'z', 0, 10, 0.01).name('Length').listen();
+  
+  // Rotation
+  geometrySettings.add(guiObj, 'rotationSpeedX', 0, 0.2, 0.005).listen();
+  geometrySettings.add(guiObj, 'rotationSpeedY', 0, 0.2, 0.005).listen();
+  geometrySettings.add(guiObj, 'rotationSpeedZ', 0, 0.2, 0.005).listen();
+  
+  // Translation
+  geometrySettings.add(guiObj, 'translationSpeedX', 0, 10, 0.01).listen();
+  geometrySettings.add(guiObj, 'translationSpeedY', 0, 10, 0.01).listen();
+  geometrySettings.add(guiObj, 'translationSpeedZ', 0, 10, 0.01).listen();
+  
+  // Wireframe setting
+  geometrySettings.add(dodecahedron.material, 'wireframe').listen();
+  
+  // Reset buttons
+  geometrySettings.add(guiObj, 'resetCamera').name('Reset Camera');
+  geometrySettings.add(guiObj, 'resetGeometry').name('Reset Geometry');
+  
+  geometrySettings.open();
+  
+  cameraSettings.add(camera, 'fov', 30, 180).name('Field of Vision')
+  cameraSettings.open();
+  
+  
   panel.open();
 }
 
@@ -88,16 +144,27 @@ function createGuiObject() {
 }
 
 function render() {
-    t += 0.01;
-    requestAnimationFrame(render);
-    dodecahedron.rotation.x += guiObj.rotationSpeedX;
-    dodecahedron.rotation.y += guiObj.rotationSpeedY;
-    dodecahedron.rotation.z += guiObj.rotationSpeedZ;
-    dodecahedron.position.x = -7*Math.sin(t*guiObj.translationSpeedX);
-    dodecahedron.position.y = -7*Math.sin(t*guiObj.translationSpeedY);
-    dodecahedron.position.z = -7*Math.sin(t*guiObj.translationSpeedZ);
-    controls.update();
-    renderer.render(scene, camera);
+  t += 0.01;
+  requestAnimationFrame(render);
+  
+  // Skybox
+  skybox.rotation.x += 0.005;
+  skybox.rotation.y += 0.005;
+  
+  // Geometry
+  dodecahedron.rotation.x += guiObj.rotationSpeedX;
+  dodecahedron.rotation.y += guiObj.rotationSpeedY;
+  dodecahedron.rotation.z += guiObj.rotationSpeedZ;
+  dodecahedron.position.x = -7*Math.sin(t*guiObj.translationSpeedX);
+  dodecahedron.position.y = -7*Math.sin(t*guiObj.translationSpeedY);
+  dodecahedron.position.z = -7*Math.sin(t*guiObj.translationSpeedZ);
+  controls.update();
+  renderer.render(scene, camera);
+  
+
+    // Camera
+    camera.updateProjectionMatrix();
+    // camera.position.set(1200, -250, 2000);
 
 }
 render();
