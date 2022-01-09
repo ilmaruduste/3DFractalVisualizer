6f736db3d89a6cdf68a003010533827674e8a41e
Ilmar Uduste <ilmaruduste@gmail.com>
2022-01-09 23:01:15 +0200

    Add initial fractal

--- src/main.js
+++ src/main.js
@@ -50,15 +50,32 @@ scene.add(skybox);
 
 // Geometry
 var guiObj = createGuiObject();
-var strangeGeometry = new THREE.DodecahedronGeometry(guiObj.geometrySize);
-var lambertMaterial = new THREE.MeshLambertMaterial({color: 0x0095DD});
-var dodecahedron = new THREE.Mesh(strangeGeometry, lambertMaterial);
-// dodecahedron.position.x = 25;
-scene.add(dodecahedron);
+// var strangeGeometry = new THREE.DodecahedronGeometry(guiObj.geometrySize);
+// var lambertMaterial = new THREE.MeshLambertMaterial({color: 0x0095DD});
+// var dodecahedron = new THREE.Mesh(strangeGeometry, lambertMaterial);
+// // dodecahedron.position.x = 25;
+// scene.add(dodecahedron);
+
+// Create the menger sponge and center it (kind of) on the page
+var parent = new THREE.Object3D();
+parent.position.set(0, 0, 0);
+scene.add(parent);
+
+// Render the sponge
+var width = 80;
+var last = 2; 
+mengerSponge(-3 * width, -3 * width, 0, width, 0, last);
+requestAnimationFrame(render);
 
 var light = new THREE.PointLight(0xFFFFFF);
 light.position.set(-10, 15, 50);
 scene.add(light);
+
+var light = new THREE.PointLight(0xFFFFFF);
+light.position.set(10, -15, -50);
+scene.add(light);
+
+scene.background = new THREE.Color(0xFFFFFF);
 
 var t = 0;
 
@@ -76,9 +93,9 @@ function createPanel() {
   // folder1.add(dodecahedron.rotation, 'z', 0, 3.14 * 2);
   
   // Scale
-  geometrySettings.add(dodecahedron.scale, 'x', 0, 10, 0.01).name('Width').listen();
-  geometrySettings.add(dodecahedron.scale, 'y', 0, 10, 0.01).name('Height').listen();
-  geometrySettings.add(dodecahedron.scale, 'z', 0, 10, 0.01).name('Length').listen();
+  geometrySettings.add(parent.scale, 'x', 0, 10, 0.01).name('Width').listen();
+  geometrySettings.add(parent.scale, 'y', 0, 10, 0.01).name('Height').listen();
+  geometrySettings.add(parent.scale, 'z', 0, 10, 0.01).name('Length').listen();
   
   // Rotation
   geometrySettings.add(guiObj, 'rotationSpeedX', 0, 0.2, 0.005).listen();
@@ -91,7 +108,7 @@ function createPanel() {
   geometrySettings.add(guiObj, 'translationSpeedZ', 0, 10, 0.01).listen();
   
   // Wireframe setting
-  geometrySettings.add(dodecahedron.material, 'wireframe').listen();
+  // geometrySettings.add(parent.material, 'wireframe').listen();
   
   // Reset buttons
   geometrySettings.add(guiObj, 'resetCamera').name('Reset Camera');
@@ -143,6 +160,95 @@ function createGuiObject() {
   }
 }
 
+
+/**
+ * Construct sponge
+ * @param x       - starting x position for the sponge
+ * @param y       - starting y position for the sponge
+ * @param z       - starting z position for the sponge
+ * @param width   - width of the sponge to be constructed
+ * @param current - current level of recursive depth in constructing the sponge
+ * @param last    - last level of recursive depth to be reached
+ */
+ function mengerSponge(x, y, z, width, current, last) {
+  // Error checking
+  if (last < 0) {
+    alert("Invalid negative last parameter");
+    return;
+  } else if (last > 5) {
+    alert("Last parameter will cause too much lag");
+    return;
+  } else if (last == 0) {
+    cube(0, 0, 0, x, y, z, width * 3);
+    return;
+  }
+
+  // iterate over the x axis
+  for (var i = 1; i <= 3; i++) {
+    // iterate over the y axis
+    for (var j = 1; j <= 3; j++) {
+      // iterate over the z axis
+      for (var k = 1; k <= 3; k++) {
+        // count the number of overlaps between x, y, and z coordinates
+        // relative to the middle third of each cube
+        var num2 = 0;
+        if (i == 2) num2++;
+        if (j == 2) num2++;
+        if (k == 2) num2++;
+
+        // If there are less than 2 overlaps, then there should be a mengerSponge
+        // in the specified area
+        if (num2 < 2) {
+          // Recurse further if there are more levels
+          if (current < last - 1) {
+            mengerSponge(
+              (x + i * width),
+              (y + j * width),
+              (z + k * width),
+              (width / 3),
+              (current + 1),
+              last
+            );
+          } else if (current == last - 1) {
+            // Otherwise draw a cube in the specified location
+            cube(i, j, k, x, y, z, width);
+          }
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Draw a cube of the passed in width in the passed in coordinates
+ * @param i     - x index out of 2 of the cubes in the sponge
+ * @param j     - y index out of 2 of the cubes in the sponge
+ * @param k     - z index out of 2 of the cubes in the sponge
+ * @param x     - starting x pixel index of the sponge
+ * @param y     - starting y pixel index of the sponge
+ * @param z     - starting z pixel index of the sponge
+ * @param width - width of the cube to be added
+ */
+function cube(i, j, k, x, y, z, width) {
+  // create a cube of the passed in width
+  var geometry = new THREE.BoxGeometry(width, width, width);
+  var material = new THREE.MeshPhongMaterial({color: 0x0095DD});
+  var cube = new THREE.Mesh(geometry, material);
+
+  // Set the position of the cube
+  cube.position.set(
+    x + ((i + 1) * width),
+    y + ((j + 1) * width),
+    z + ((k + 1) * width)
+  );
+
+  // Add the cube to the screen
+  parent.add(cube);
+}
+
+
+// RENDER
+
 function render() {
   t += 0.01;
   requestAnimationFrame(render);
@@ -154,12 +260,12 @@ function render() {
   skybox.rotation.z += skyboxRotationCoef*guiObj.skyboxRotationSpeedZ;
 
   // Geometry
-  dodecahedron.rotation.x += guiObj.rotationSpeedX;
-  dodecahedron.rotation.y += guiObj.rotationSpeedY;
-  dodecahedron.rotation.z += guiObj.rotationSpeedZ;
-  dodecahedron.position.x = -7*Math.sin(t*guiObj.translationSpeedX);
-  dodecahedron.position.y = -7*Math.sin(t*guiObj.translationSpeedY);
-  dodecahedron.position.z = -7*Math.sin(t*guiObj.translationSpeedZ);
+  parent.rotation.x += guiObj.rotationSpeedX;
+  parent.rotation.y += guiObj.rotationSpeedY;
+  parent.rotation.z += guiObj.rotationSpeedZ;
+  parent.position.x = -7*Math.sin(t*guiObj.translationSpeedX);
+  parent.position.y = -7*Math.sin(t*guiObj.translationSpeedY);
+  parent.position.z = -7*Math.sin(t*guiObj.translationSpeedZ);
   controls.update();
   renderer.render(scene, camera);
   
