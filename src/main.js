1d72ba9cb6cbbe381442065cefedbfc236ec4f71
Ilmar Uduste <ilmaruduste@gmail.com>
2022-01-09 18:32:10 +0200

    Add UI and geometry controls

--- src/main.js
+++ src/main.js
@@ -22,7 +22,8 @@ controls.update();
 // cube.rotation.set(0.4, 0.2, 0);
 // scene.add(cube);
 
-var strangeGeometry = new THREE.DodecahedronGeometry(7);
+var guiObj = createGuiObject();
+var strangeGeometry = new THREE.DodecahedronGeometry(guiObj.geometrySize);
 var lambertMaterial = new THREE.MeshLambertMaterial({color: 0x0095DD});
 var dodecahedron = new THREE.Mesh(strangeGeometry, lambertMaterial);
 // dodecahedron.position.x = 25;
@@ -33,14 +34,63 @@ light.position.set(-10, 15, 50);
 scene.add(light);
 
 var t = 0;
+
+
+function createPanel() {
+  const panel = new dat.GUI( {width: 310});
+  
+  const folder1 = panel.addFolder('Geometry');
+
+  
+  // folder1.add(dodecahedron.rotation, 'x', 0, 3.14 * 2);
+  // folder1.add(dodecahedron.rotation, 'y', 0, 3.14 * 2);
+  // folder1.add(dodecahedron.rotation, 'z', 0, 3.14 * 2);
+  folder1.add(dodecahedron.scale, 'x', 0, 10, 0.01).name('Width').listen();
+  folder1.add(dodecahedron.scale, 'y', 0, 10, 0.01).name('Height').listen();
+  folder1.add(dodecahedron.scale, 'z', 0, 10, 0.01).name('Length').listen();
+  folder1.add(guiObj, 'rotationSpeedX', 0, 3, 0.01).listen();
+  folder1.add(guiObj, 'rotationSpeedY', 0, 3, 0.01).listen();
+  folder1.add(guiObj, 'rotationSpeedZ', 0, 3, 0.01).listen();
+  folder1.add(guiObj, 'translationSpeedX', 0, 10, 0.01).listen();
+  folder1.add(guiObj, 'translationSpeedY', 0, 10, 0.01).listen();
+  folder1.add(guiObj, 'translationSpeedZ', 0, 10, 0.01).listen();
+  folder1.add(dodecahedron.material, 'wireframe').listen();
+  folder1.add(guiObj, 'resetCamera').name('Reset Camera');
+  folder1.open();
+  panel.open();
+}
+
+function createGuiObject() {
+  return {
+    rotationSpeedX: 0,
+    rotationSpeedY: 0.01,
+    rotationSpeedZ: 0,
+    translationSpeedX: 0,
+    translationSpeedY: 2,
+    translationSpeedZ: 0,
+    geometrySize: 7,
+    resetCamera: function() {
+      camera.position.x = 0;
+      camera.position.y = 0;
+      camera.position.z = 50;
+    }
+  }
+}
+
 function render() {
     t += 0.01;
     requestAnimationFrame(render);
     // cube.rotation.y += 0.01;
     // torus.scale.y = Math.abs(Math.sin(t));
-    dodecahedron.rotation.y += 0.01;
-    dodecahedron.position.y = -7*Math.sin(t*2);
+    dodecahedron.rotation.x += guiObj.rotationSpeedX;
+    dodecahedron.rotation.y += guiObj.rotationSpeedY;
+    dodecahedron.rotation.z += guiObj.rotationSpeedZ;
+    dodecahedron.position.x = -7*Math.sin(t*guiObj.translationSpeedX);
+    dodecahedron.position.y = -7*Math.sin(t*guiObj.translationSpeedY);
+    dodecahedron.position.z = -7*Math.sin(t*guiObj.translationSpeedZ);
     controls.update();
     renderer.render(scene, camera);
+
 }
 render();
+createPanel();
